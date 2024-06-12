401b2029b28d4189b3a5cf70908f37120d0bd1ed
Parsa <parsaalizadeh2022@gmail.com>
2024-06-13 03:41:25 +0430

    some changes

--- emailsender/settings.py
+++ emailsender/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from sendingemailapp.views import emailname,password
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,6 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='parsaalizadeh2040@gmail.com'
-EMAIL_HOST_PASSWORD ='xckfqszzqklbkcpz'
+EMAIL_HOST_USER = emailname()
+EMAIL_HOST_PASSWORD = password()
 EMAIL_USE_TLS = True
--- sendingemailapp/views.py
+++ sendingemailapp/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from django.core.mail import EmailMessage
 from django.http import HttpResponse
+password=""
+emailname=""
 def mainpage(request):
     if request.method=="POST":
         subject_t=request.POST.get('subject')
         Message_t=request.POST.get('Message')
+        email_t=request.POST.get('email')
+        password_t=request.POST.get('password')
+        global password
+        password=password_t
         if Message_t and subject_t is not None:
-            email=EmailMessage(
-                subject_t,
-                Message_t,
-                "parsaalizadeh2022@gmail.com",
-                ["parsaalizadeh2040@gmail.com","parsaalizadeh2022@gmail.com"],
-            ).send()
+            s=["parsaalizadeh2040@gmail.com","parsaalizadeh202@gmail.com"]
+            for x in s:
+                email=EmailMessage(
+                    subject_t,
+                    Message_t,
+                    "parsaalizadeh2040@gmail.com",
+                    [x],
+                ).send()
     return render(request,"mainpage.html")
+def emailname():
+    return "parsaalizadeh2040@gmail.com"
+
+def password():
+    return "xckfqszzqklbkcpz"
+
+#'xckfqszzqklbkcpz'
