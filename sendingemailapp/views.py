98919764f8ea2c835eed2955635c5bac00819c6e
Parsa <parsaalizadeh2022@gmail.com>
2024-06-13 02:05:17 +0430

    create template and run sending email with it

--- sendingemailapp/views.py
+++ sendingemailapp/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.core.mail import EmailMessage
 from django.http import HttpResponse
 def mainpage(request):
-    email=EmailMessage(
-        "parsa",
-        "bye",
-        "parsaalizadeh2022@gmail.com",
-        ["parsaalizadeh2040@gmail.com","parsaalizadeh2022@gmail.com"],
-    ).send()
-    return HttpResponse("your email was send")
+    if request.method=="POST":
+        subject_t=request.POST.get('subject')
+        Message_t=request.POST.get('Message')
+        if Message_t and subject_t is not None:
+            email=EmailMessage(
+                subject_t,
+                Message_t,
+                "parsaalizadeh2022@gmail.com",
+                ["parsaalizadeh2040@gmail.com","parsaalizadeh2022@gmail.com"],
+            ).send()
+    return render(request,"mainpage.html")
