a1344ef7e52e52919620352aab2e5efd85de619e
Parsa <parsaalizadeh2022@gmail.com>
2024-06-13 15:21:55 +0430

    complete sending email with form

--- sendingemailapp/views.py
+++ sendingemailapp/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render
 from django.core.mail import EmailMessage
 from django.http import HttpResponse
 from django.conf import settings
-password=["bjgpyykfvowtuuiq",]
-emailname=[]
 def mainpage(request):
     if request.method=="POST":
         subject_t=request.POST.get('subject')
         Message_t=request.POST.get('Message')
         email_t=request.POST.get('email')
+        email_re=request.POST.get('email_re')
         password_t=request.POST.get('password')
         settings.EMAIL_HOST_USER=email_t
         settings.EMAIL_HOST_PASSWORD=password_t
         if Message_t and subject_t is not None:
-            s=["parsaalizadeh2040@gmail.com","parsaalizadeh202@gmail.com"]
+            # s=["gmail@gmail.com","gmail@gmail.com"]
+            s=email_re.split(',')
             for x in s:
                 email=EmailMessage(
                     subject_t,
                     Message_t,
-                    "parsaalizadeh2022@gmail.com",
+                    "gmail@gmail.com",
                     [x],
                 ).send()
     return render(request,"mainpage.html")
 #'bjgpyykfvowtuuiq'
-# rxwgyxsleedddkcg 
+# rxwgyxsleedddkcg ,wwsgapgnbwzrijgm 
